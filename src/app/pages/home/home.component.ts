2bf0b7cccfd478014a1b6c94134d179d6f5fef7e
Sirine Kahweji <sirinekahweji5@gmail.com>
2025-08-04 21:46:08 +0200

    partie who i'm

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component,OnInit } from '@angular/core';
 import { NavbarComponent } from './../../components/navbar/navbar.component';
+import { AboutComponent } from './../../components/about/about.component';
 import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,CommonModule],
+  imports: [NavbarComponent,AboutComponent,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
